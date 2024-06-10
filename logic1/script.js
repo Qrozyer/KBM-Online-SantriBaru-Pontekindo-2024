ae607fa2e71ac9f3124e1fbde83a7fdf2dee2f4e
Qrozyer <qrozyer@gmail.com>
2024-06-10 09:37:22 +0700

    update

--- logic1/script.js
+++ logic1/script.js
@@ -1,13 +1,4 @@
-document.write("<h1>Tes Logika</h1>");
-document.write("<h3>Intro</h3>");
-
-for(let y = 1; y <=9; y++){
-    for(let x = 1; x <=9; x++){
-        document.writeln('*')
-    }
-    document.write('<br>')
-}
-        
+document.write("<h1>Tes Logika</h1>");        
 document.write("<h2>Logic 1</h2>");
 document.write("<h3>Soal 1</h3>");
 
--- script.js
+++ script.js
@@ -7,17 +7,3 @@ for(let y = 1; y <=9; y++){
     }
     document.write('<br>')
 }
-        
-document.write("<h2>Logic 1</h2>");
-document.write("<h3>Soal 1</h3>");
-
-for(let y = 1; y <= 9; y++){
-    for(let x = 1; x <= 9; x++){
-        if (x == y) {
-            document.writeln('*')
-        }else {
-            document.writeln('~')
-        }
-    }
-    document.write('<br>')
-}
